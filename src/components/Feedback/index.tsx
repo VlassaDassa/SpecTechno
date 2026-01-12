56f14c9e5c1f5bdeda25cf6dfd96531f5463ae74
vlad <vlad.sad28@yandex.ru>
2026-01-12 10:11:59 +0300

    Form verification

--- src/components/Feedback/index.tsx
+++ src/components/Feedback/index.tsx
@@ -10,12 +10,114 @@ import rectangle from './../../assets/images/rectangle.svg';
 
 import './index.scss';
 
+
+type Errors = {
+    name?: string;
+    email?: string;
+    message?: string;
+}
+
 const Feedback: React.FC = () => {
     const [counter, setCounter] = useState<number>(0);
+    const [showSuccess, setShowSuccess] = useState(false);
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    // Processing a hidden clone of textarea
     const textareaRef = useRef<HTMLTextAreaElement>(null);
     const shadowRef = useRef<HTMLDivElement>(null);
 
+    // Form verification
+    const [errors, setErrors] = useState<Errors>({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const nameRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+        
+        if (isSubmitted) {
+            validateForm();
+        }
+    };
+
+    // Form verification
+    const validateName = (value: string) => {
+        if (value.trim().length < 4) {
+            return 'Имя должно содержать минимум 4 символа';
+        }
+    };
+
+    const validateEmail = (value: string) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(value)) {
+            return 'Введите корректный email';
+        }
+    };
+
+    const validateMessage = (value: string) => {
+        if (value.trim().length < 10) {
+            return 'Сообщение должно быть не менее 10 символов';
+        }
+    };
+
+    const validateForm = () => {
+        const name = nameRef.current?.value || '';
+        const email = emailRef.current?.value || '';
+        const message = textareaRef.current?.value || '';
+    
+        const newErrors: Errors = {
+            name: validateName(name),
+            email: validateEmail(email),
+            message: validateMessage(message),
+        };
+
+        setErrors(newErrors);
+
+        return !Object.values(newErrors).some(Boolean);
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setIsSubmitted(true);
+
+        if (!validateForm()) return;
+
+        setShowSuccess(true);
+        
+        setFormData({
+            name: '',
+            email: '',
+            message: ''
+        });
+
+        // Clear form
+        setTimeout(() => {
+            setIsSubmitted(false);
+            setCounter(0);
+            setShowSuccess(false);
+        }, 5000);
+        
+        // Hide push
+        setTimeout(() => {
+            setShowSuccess(false);
+        }, 5000);
+        
+        // sendDataToServer(formData);
+    }
+
+    const handleLiveValidation = () => {
+        if (!isSubmitted) return;
+        validateForm();
+    }
+
+    
+
+    // Processing a hidden clone of textarea
     const handleInput = () => {
         const textarea = textareaRef.current;
         const shadow = shadowRef.current;
@@ -49,36 +151,56 @@ const Feedback: React.FC = () => {
 
             <div className="feedbackForm">
                 <h2 className="feedbackTitle">Обратная связь</h2>
-                <form action="#">
+                <form noValidate onSubmit={handleSubmit}>
                     <div className="fieldsContainer">
 
                         <div className="fieldWrapper">
                             <img src={profileIcon} alt="Имя" />
-                            <input type="text"  
-                                className="feedbackField" 
-                                placeholder="Ваше имя" 
+                            <input type="text" 
+                                name="name" 
+                                ref={nameRef}
+                                className={`feedbackField ${errors.name ? 'errorField' : ''}`}
+                                placeholder="Ваше имя"
+                                onInput={handleLiveValidation}
+                                onChange={handleChange}
+                                value={formData.name}
                             />
+                            {errors.name && <span className="errorMessage">{errors.name}</span>}
                         </div>
 
                         <div className="fieldWrapper">
                             <img src={emailIcon} alt="Почта" />
                             <input 
+                                name="email" 
+                                ref={emailRef}
                                 type="email" 
-                                className="feedbackField" 
-                                placeholder="Ваша почта" 
+                                className={`feedbackField ${errors.email ? 'errorField' : ''}`}
+                                placeholder="Ваша почта"
+                                onInput={handleLiveValidation}
+                                onChange={handleChange}
+                                value={formData.email}
                             />
+                            {errors.email && <span className="errorMessage">{errors.email}</span>}
                         </div>
 
                         <div className="fieldWrapper">
                             <img src={messageIcon} alt="Сообщение" />
                             <textarea
                                 ref={textareaRef}
-                                className="feedbackField feedbackTextarea"
+                                name="message"
+                                className={`feedbackField feedbackTextarea ${errors.message ? 'errorField' : ''}`}
                                 placeholder="Ваше сообщение"
-                                onInput={handleInput}
+                                value={formData.message}
+                                onInput={() => {
+                                    handleInput();
+                                    handleLiveValidation();
+                                }}
+                                onChange={handleChange}
                                 maxLength={500}
                                 style={{ overflow: 'hidden', resize: 'none', width: '100%' }}
                             />
+
+                            {errors.message && <span className="errorMessage">{errors.message}</span>}
 
                             <div
                                 className={'textareaClone'}
@@ -124,6 +246,22 @@ const Feedback: React.FC = () => {
             
             <img src={messageRectangle} className="messageRectangle messageRectangle1" />
             <img src={messageRectangle} className="messageRectangle messageRectangle2" />
+
+            <div className={`success-notification ${showSuccess ? 'show' : ''}`}>
+                <div className="success-content">
+                    <div className="success-icon">✓</div>
+                    <div className="success-text">
+                        <h3>Сообщение успешно отправлено!</h3>
+                        <p>В течение 24 часов вам придёт ответ на указанную вами почту</p>
+                    </div>
+                    <button 
+                        className="success-close"
+                        onClick={() => setShowSuccess(false)}
+                    >
+                        x
+                    </button>
+                </div>
+            </div>
 
         </section>
     )
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,10 +5,12 @@ import './index.scss';
 
 const Footer: React.FC = () => {
     return (
-        <footer className="footer container">
-            <p className="text">info@spectechno.com</p>
-            <p className="text">101000, Россия, г.Москва, Милютинский пер,. д.9, стр.2</p>
-            <p className="text">(+7 495) 22-770-308</p>
+        <footer className="footer">
+            <div className="container footerWrapper">
+                <p className="text">info@spectechno.com</p>
+                <p className="text">101000, Россия, г.Москва, Милютинский пер,. д.9, стр.2</p>
+                <p className="text">(+7 495) 22-770-308</p>
+            </div>
         </footer>
     )
 }
